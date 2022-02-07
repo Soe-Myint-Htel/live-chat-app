3ce3ad1fcbb062c8e2b110bbbae6ec48d4d7f6d2
Soe Myint Htel <gretaoto44@gmail.com>
2022-02-06 18:02:59 -0800

    make date format with date-fns library

--- src/components/chatWindow.vue
+++ src/components/chatWindow.vue
@@ -1,54 +1,66 @@
 <template>
   <div class="chat-window">
-      <div class="messages" v-for="msg in messages" :key="msg.id">
-          <div class="single">
-              <spam class="created-at">{{msg.created_at.toDate()}}</spam>
-              <spam class="name">{{msg.name}}</spam>
-              <spam class="message">{{msg.message}}</spam>
-          </div>
+    <div class="messages">
+      <div class="single" v-for="msg in formattedMessages" :key="msg.id">
+        <spam class="created-at">{{ msg.created_at }}</spam>
+        <spam class="name">{{ msg.name }}</spam>
+        <spam class="message">{{ msg.message }}</spam>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
-import { db } from '../firebase/config'
+import { ref } from "@vue/reactivity";
+import { db } from "../firebase/config";
+import {formatDistanceToNow } from "date-fns/";
+import { computed } from "@vue/runtime-core";
 export default {
-    setup(){
-        let messages = ref([]);
-        db.collection("messages").orderBy("created_at").onSnapshot((snap)=>{
-            let results = [];
-            snap.docs.forEach((doc)=>{
-                let document = {...doc.data(),id: doc.id};
-                doc.data().created_at && results.push(document);
-            })
-            messages.value = results;
-        })
-        return {messages}
-    }
-}
+  setup() {
+    let messages = ref([]);
+    let formattedMessages = computed(() => {
+      return messages.value.map((msg) => {
+        let formatTime = formatDistanceToNow(msg.created_at.toDate());
+        return { ...msg, created_at: formatTime };
+      });
+    });
+
+    db.collection("messages")
+      .orderBy("created_at")
+      .onSnapshot((snap) => {
+        let results = [];
+        snap.docs.forEach((doc) => {
+          let document = { ...doc.data(), id: doc.id };
+          doc.data().created_at && results.push(document);
+        });
+        messages.value = results;
+      });
+      
+    return { messages, formattedMessages};
+  },
+};
 </script>
 
 <style>
 .chat-window {
-        background: #fafafa;
-        padding: 30px 20px;
-      }
-      .single {
-        margin: 18px 0;
-      }
-      .created-at {
-        display: block;
-        color: #999;
-        font-size: 12px;
-        margin-bottom: 4px;
-      }
-      .name {
-        font-weight: bold;
-        margin-right: 6px;
-      }
-      .messages {
-        max-height: 400px;
-        overflow: auto;
-      }
+  background: #fafafa;
+  padding: 30px 20px;
+}
+.single {
+  margin: 18px 0;
+}
+.created-at {
+  display: block;
+  color: #999;
+  font-size: 12px;
+  margin-bottom: 4px;
+}
+.name {
+  font-weight: bold;
+  margin-right: 6px;
+}
+.messages {
+  max-height: 400px;
+  overflow: auto;
+}
 </style>
